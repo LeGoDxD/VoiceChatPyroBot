eb081c315dedfe1d9ba0336c0a624f8fa5cd767d
LeGoDxD <76858499+LeGoDxD@users.noreply.github.com>
2021-01-09 19:48:25 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 from pyrogram import filters
 
 # Get these two from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = 1555595
+API_HASH = "6482c597a54f0f49d881401f0c16e71c"
 
 # Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+TOKEN = "1554977448:AAGqfAm3-6nfHHlouRFkHn1mz3Ov0Ahy3eo"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
@@ -16,19 +16,19 @@ SUDO_USERS = [
 ]
 
 # The ID of the group where your bot streams
-GROUP = -1001402753006
+GROUP = False
 
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
 USERS_MUST_JOIN = False
 
 # Send "now playing" messages to the group
-LOG = True
+LOG = False
 
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 LANG = "en"
 
 # Max video duration allowed for user downloads in minutes
-DUR_LIMIT = 5
+DUR_LIMIT = 10
 
 # No need to touch the following.
 LOG_GROUP = GROUP if LOG else None
